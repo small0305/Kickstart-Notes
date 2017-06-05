9997342e0a38b08e3165359b9513b2337a8d8065
Small <Small@DESKTOP-KKKIVHE>
2017-06-05 23:03:21 +0800

    2017 Round B Problem A

--- 2017RoundB/ProblemA/problemA.cpp
+++ 2017RoundB/ProblemA/problemA.cpp
@@ -2,41 +2,38 @@ using namespace std;
 #include <iostream>
 #include <vector>
 
-int factorial[10000];
+int pow[10000];
 int MOD = 1000000007;
 
-int calF(int n){
-	if(n==1)
-		return 1;
-	if(factorial[n])
-		return factorial[n];
-	factorial[n] = n*calF(n-1)%MOD;
-	return factorial[n];
+void calc(){
+	pow[0] = 1;
+	for(int i = 1; i<10000; i++){
+		pow[i] = 2 * pow[i-1] % MOD;
+	}
 }
-int C(int n,int r){
-	return(calF(n)%MOD/(calF(r)%MOD)/(calF(n-r)%MOD)%MOD);
-}
+
 int encoder(vector<int> &nums){
-	int ans = 0,n = nums.size();
-	for(int i = 1;i<n;i++){
-		for(int j = i;j<n;j++){
-			ans += j*(nums[j] - nums[n-j-1]);
-			cout<<ans<<' ';
-		}
+	long long ans = 0;
+	int n = nums.size();
+	for(int i = n-1; i>=0; i--){
+		ans += (long long)pow[i] * (long long)(nums[i] - nums[n-i-1]);
+		ans %= MOD;
+		ans += MOD;
+		ans %= MOD;
 	}
 	return ans;
 }
 int main(){
+    calc();
 	int T;
-	cin>>T;
-	for(int j = 1;j<=T;j++){
+	cin >> T;
+	for(int j = 1; j<=T; j++){
 		int N;
-		cin>>N;
+		cin >> N;
 		vector<int> nums(N,0);
-		for(int i = 0;i<N;i++){
-			cin>>nums[i];
-		}
-		cout<<"Case #"<<j<<": "<<encoder(nums)<<endl;
+		for(int i = 0; i<N; i++)
+			cin >> nums[i];
+		cout << "Case #"<<j<<": "<<encoder(nums)<<endl;
 	}
 	return 0;
 }
--- 2017RoundB/ProblemA/problemA.md
+++ 2017RoundB/ProblemA/problemA.md
@@ -40,18 +40,18 @@ using namespace std;
 int pow[10000];
 int MOD = 1000000007;
 
-void clac(){
+void calc(){
 	pow[0] = 1;
-	for(int i = 1;i<10000;i++){
-		pow[i] = 2*pow[i-1];
+	for(int i = 1; i<10000; i++){
+		pow[i] = 2 * pow[i-1] % MOD;
 	}
 }
 
 int encoder(vector<int> &nums){
 	long long ans = 0;
 	int n = nums.size();
-	for(int i = n-1;i>0;i--){
-		ans += (long long)pow[i]*(long long)(nums[i] - nums[n-i-1]);
+	for(int i = n-1; i>=0; i--){
+		ans += (long long)pow[i] * (long long)(nums[i] - nums[n-i-1]);
 		ans %= MOD;
 		ans += MOD;
 		ans %= MOD;
@@ -59,17 +59,18 @@ int encoder(vector<int> &nums){
 	return ans;
 }
 int main(){
+    calc();
 	int T;
-	cin>>T;
-	for(int j = 1;j<=T;j++){
+	cin >> T;
+	for(int j = 1; j<=T; j++){
 		int N;
-		cin>>N;
+		cin >> N;
 		vector<int> nums(N,0);
-		for(int i = 0;i<N;i++){
-			cin>>nums[i];
-		}
-		cout<<"Case #"<<j<<": "<<encoder(nums)<<endl;
+		for(int i = 0; i<N; i++)
+			cin >> nums[i];
+		cout << "Case #"<<j<<": "<<encoder(nums)<<endl;
 	}
 	return 0;
 }
+
 ```
